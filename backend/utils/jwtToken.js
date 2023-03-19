0dc67220acf589db94e58801d424b32b70508634
SharmaYashasvi <202011079@diu.iiitvadodara.ac.in>
2023-03-19 23:25:33 +0530

    commit

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,9 +5,8 @@ const sendToken = (user, statusCode, res) => {
 
   // options for cookie
   const options = {
-    expires: new Date(
-      Number(Date.now()) + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
+    
+    expires: null,
     httpOnly: true,
   };
 
